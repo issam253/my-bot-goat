51535fb0b9b79ed3d0d075d2199df0f3e0bb6058
issam253 <166600559+issam253@users.noreply.github.com>
2024-09-15 06:55:36 +0000

    Update antichangeinfobox.js

--- scripts/cmds/antichangeinfobox.js
+++ scripts/cmds/antichangeinfobox.js
@@ -2,14 +2,14 @@ const { getStreamFromURL, uploadImgbb } = global.utils;
 
 module.exports = {
 	config: {
-		name: "antichangeinfobox",
+		name: "حماية",
 		version: "1.9",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
 		description: {
 			vi: "Bật tắt chức năng chống thành viên đổi thông tin box chat của bạn",
-			en: "Turn on/off anti change info box"
+			en: "حماية معلومات المجموعة من التغيير"
 		},
 		category: "box chat",
 		guide: {
@@ -18,11 +18,11 @@ module.exports = {
 				+ "\n   {pn} nickname [on | off]: chống đổi nickname trong box chat"
 				+ "\n   {pn} theme [on | off]: chống đổi theme (chủ đề) box chat"
 				+ "\n   {pn} emoji [on | off]: chống đổi trạng emoji box chat",
-			en: "   {pn} avt [on | off]: anti change avatar box chat"
-				+ "\n   {pn} name [on | off]: anti change name box chat"
-				+ "\n   {pn} nickname [on | off]: anti change nickname in box chat"
-				+ "\n   {pn} theme [on | off]: anti change theme (chủ đề) box chat"
-				+ "\n   {pn} emoji [on | off]: anti change emoji box chat"
+			en: "   {pn} الصورة [تشغيل | ايقاف]: حماية صورة المجموعة"
+				+ "\n   {pn} الاسم [تشغيل | ايقاف]: حمار اسم المجموعة"
+				+ "\n   {pn} كنية [تشغيل | ايقاف]: لحماية كنية الأعضاء "
+				+ "\n   {pn} الثيم [تشغيل | ايقاف]: لحماية ثيم المجموعة"
+				+ "\n   {pn} ايموجي [تشغيل | ايقاف]: لحماية ايموجي المجموعة"
 		}
 	},
 
@@ -47,34 +47,34 @@ module.exports = {
 			antiChangeEmojiAlreadyOn: "Hiện tại box chat của bạn đang bật chức năng cấm thành viên đổi emoji"
 		},
 		en: {
-			antiChangeAvatarOn: "Turn on anti change avatar box chat",
-			antiChangeAvatarOff: "Turn off anti change avatar box chat",
-			missingAvt: "You have not set avatar for box chat",
-			antiChangeNameOn: "Turn on anti change name box chat",
-			antiChangeNameOff: "Turn off anti change name box chat",
-			antiChangeNicknameOn: "Turn on anti change nickname box chat",
-			antiChangeNicknameOff: "Turn off anti change nickname box chat",
-			antiChangeThemeOn: "Turn on anti change theme box chat",
-			antiChangeThemeOff: "Turn off anti change theme box chat",
-			antiChangeEmojiOn: "Turn on anti change emoji box chat",
-			antiChangeEmojiOff: "Turn off anti change emoji box chat",
-			antiChangeAvatarAlreadyOn: "Your box chat is currently on anti change avatar",
-			antiChangeAvatarAlreadyOnButMissingAvt: "Your box chat is currently on anti change avatar but your box chat has not set avatar",
-			antiChangeNameAlreadyOn: "Your box chat is currently on anti change name",
-			antiChangeNicknameAlreadyOn: "Your box chat is currently on anti change nickname",
-			antiChangeThemeAlreadyOn: "Your box chat is currently on anti change theme",
-			antiChangeEmojiAlreadyOn: "Your box chat is currently on anti change emoji"
+			antiChangeAvatarOn: "تم تشغيل حماية صورة المجموعة.",
+			antiChangeAvatarOff: "تم إيقاف تشغيل حماية المجموعة",
+			missingAvt: "اصلا مافي صورة 🤡",
+			antiChangeNameOn: "تم تشغيل حماية الأسم",
+			antiChangeNameOff: "تم ايقاف حماية الأسم.",
+			antiChangeNicknameOn: "تم تشغيل حماية الكنية",
+			antiChangeNicknameOff: "تم ايقاف حماية الكنية",
+			antiChangeThemeOn: "تم تشغيل حماية الثيم",
+			antiChangeThemeOff: "تم ايقاف حماية الثيم",
+			antiChangeEmojiOn: "تم تشغيل حماية الايموجي",
+			antiChangeEmojiOff: "تم ايقاف حماية المجموعة",
+			antiChangeAvatarAlreadyOn: "حماية الصورة قيد التشغيل",
+			antiChangeAvatarAlreadyOnButMissingAvt: "دردشة الصندوق الخاصة بك حاليًا في وضع عدم التغيير ولكن دردشة الصندوق الخاصة بك لم يتم تعيين الصورة الرمزية لها",
+			antiChangeNameAlreadyOn: "حماية الأسم قيد التشغيل",
+			antiChangeNicknameAlreadyOn: "حماية الكنية قيد التشغيل",
+			antiChangeThemeAlreadyOn: "حماية الثيم قيد التشغيل",
+			antiChangeEmojiAlreadyOn: "حماية الايموجي قيد التشغيل"
 		}
 	},
 
 	onStart: async function ({ message, event, args, threadsData, getLang }) {
-		if (!["on", "off"].includes(args[1]))
+		if (!["تشغيل", "ايقاف"].includes(args[1]))
 			return message.SyntaxError();
 		const { threadID } = event;
 		const dataAntiChangeInfoBox = await threadsData.get(threadID, "data.antiChangeInfoBox", {});
 		async function checkAndSaveData(key, data) {
-			// dataAntiChangeInfoBox[key] = args[1] === "on" ? data : false;
-			if (args[1] === "off")
+			// dataAntiChangeInfoBox[key] = args[1] === "تشغيل" ? data : false;
+			if (args[1] === "ايقاف")
 				delete dataAntiChangeInfoBox[key];
 			else
 				dataAntiChangeInfoBox[key] = data;
@@ -83,7 +83,7 @@ module.exports = {
 			message.reply(getLang(`antiChange${key.slice(0, 1).toUpperCase()}${key.slice(1)}${args[1].slice(0, 1).toUpperCase()}${args[1].slice(1)}`));
 		}
 		switch (args[0]) {
-			case "avt":
+			case "الصورة":
 			case "avatar":
 			case "image": {
 				const { imageSrc } = await threadsData.get(threadID);
@@ -93,22 +93,22 @@ module.exports = {
 				await checkAndSaveData("avatar", newImageSrc.image.url);
 				break;
 			}
-			case "name": {
+			case "الاسم": {
 				const { threadName } = await threadsData.get(threadID);
 				await checkAndSaveData("name", threadName);
 				break;
 			}
-			case "nickname": {
+			case "الكنية": {
 				const { members } = await threadsData.get(threadID);
 				await checkAndSaveData("nickname", members.map(user => ({ [user.userID]: user.nickname })).reduce((a, b) => ({ ...a, ...b }), {}));
 				break;
 			}
-			case "theme": {
+			case "الثيم": {
 				const { threadThemeID } = await threadsData.get(threadID);
 				await checkAndSaveData("theme", threadThemeID);
 				break;
 			}
-			case "emoji": {
+			case "الايموجي": {
 				const { emoji } = await threadsData.get(threadID);
 				await checkAndSaveData("emoji", emoji);
 				break;
